ecb58d7ad3790be7cda0c1b677bc4869629bbba8
Akin Oladeji <lahdayg@yahoo.com>
2020-07-03 16:09:50 +0100

    This is my first commit

---
+++ LearnWithPrideProject/Features/DataDrivenWithParametirization.feature
@@ -0,0 +1,13 @@
+﻿Feature: DataDriven
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+@mytag
+Scenario Outline: Data Driven With Examples
+	Given I navigate to "https://angularjs.realworld.io/#/register"
+	When I enter Username text "learnwithpride"
+	And I enter email test "learnwithpride@demo.com"
+	And I enter password text "PasswordSecure"
+	#And I click on Sign up button
+	#Then I am logged in with my username displayed
---
+++ LearnWithPrideProject/StepDefinition/DataDrivenSteps.cs
@@ -0,0 +1,66 @@
+﻿using LearnWithPrideProject.PageObject;
+using NUnit.Framework;
+using System;
+using System.Threading;
+using TechTalk.SpecFlow;
+
+namespace LearnWithPrideProject.StepDefinition
+{
+    [Binding]
+    public class DataDrivenSteps
+    {
+
+        DataDrivenWithParameterPage dataDrivenWithParameterPage;
+
+        public DataDrivenSteps()
+        {
+            dataDrivenWithParameterPage = new DataDrivenWithParameterPage();
+        }
+
+
+
+
+
+
+
+
+        [Given(@"I navigate to ""(.*)""")]
+        public void GivenINavigateTo(string url)
+        {
+            dataDrivenWithParameterPage.NavigateToWebsite(url);
+                    
+                                                                                            
+        }
+        
+        [When(@"I enter Username text ""(.*)""")]
+        public void WhenIEnterUsernameText(string username)
+        {
+            dataDrivenWithParameterPage.EnterUsername(username);
+        }
+        
+        [When(@"I enter email test ""(.*)""")]
+        public void WhenIEnterEmailTest(string email)
+        {
+            dataDrivenWithParameterPage.EnterEmail(email);
+        }
+        
+        [When(@"I enter password text ""(.*)""")]
+        public void WhenIEnterPasswordText(string password)
+        {
+            dataDrivenWithParameterPage.EnterPassword(password);
+        }
+        
+        [When(@"I click on Sign up button")]
+        public void WhenIClickOnSignUpButton()
+        {
+            dataDrivenWithParameterPage.ClickOnSignUpButton();
+        }
+        
+        [Then(@"I am logged in with my username displayed")]
+        public void ThenIAmLoggedInWithMyUsernameDisplayed()
+        {
+            Thread.Sleep(5000);
+            Assert.That(dataDrivenWithParameterPage.IsUserLogInDisplayed); //NUnit Assertion
+        }
+    }
+}
